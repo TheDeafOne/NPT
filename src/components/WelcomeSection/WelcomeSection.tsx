75e6e1b9552b7f3c20f03db0179044b767819ce8
TheDeafOne <woodburnkb20@gcc.edu>
2023-06-04 16:55:11 -0500

    base welcome section

---
+++ src/components/WelcomeSection/WelcomeSection.tsx
@@ -0,0 +1,13 @@
+import Section from "../Section"
+
+const WelcomeSection = () => {
+    return (
+        <Section>
+            <div>
+                test
+            </div>
+        </Section>
+    )
+}
+
+export default WelcomeSection
