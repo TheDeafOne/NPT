f7b966341bf88c0e0b8310a4ea9812a655ffbfe5
TheDeafOne <woodburnkb20@gcc.edu>
2023-06-07 22:21:55 -0500

    base experience section

---
+++ src/components/ExperienceSection/ExperienceSection.tsx
@@ -0,0 +1,13 @@
+import Section from "../Section"
+
+const ExperienceSection = () => {
+    return (
+        <Section>
+            <div className="bg-black text-white">
+                experience
+            </div>
+        </Section>
+    )
+}
+
+export default ExperienceSection
