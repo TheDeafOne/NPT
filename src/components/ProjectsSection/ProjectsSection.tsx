bbbdd86082c29adb7c120884b0547727bba9628f
TheDeafOne <woodburnkb20@gcc.edu>
2023-06-07 22:22:07 -0500

    base project section

---
+++ src/components/ProjectsSection/ProjectsSection.tsx
@@ -0,0 +1,13 @@
+import Section from "../Section"
+
+const ProjectsSection = () => {
+    return (
+        <Section>
+            <div className="bg-black text-white">
+                projects
+            </div>
+        </Section>
+    )
+}
+
+export default ProjectsSection
