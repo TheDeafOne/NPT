503e925a3ee8b9c88fdcb1352d97d04ddd779183
TheDeafOne <woodburnkb20@gcc.edu>
2023-06-07 22:21:41 -0500

    base about section

---
+++ src/components/AboutSection/AboutSection.tsx
@@ -0,0 +1,13 @@
+import Section from "../Section"
+
+const AboutSection = () => {
+    return (
+        <Section>
+            <div className="bg-black text-white">
+                about
+            </div>
+        </Section>
+    )
+}
+
+export default AboutSection
