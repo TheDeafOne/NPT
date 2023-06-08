6e095d3e2da9d895df34f223cdcefbfdd010952d
TheDeafOne <woodburnkb20@gcc.edu>
2023-06-07 22:22:13 -0500

    base skills section

---
+++ src/components/SkillsSection/SkillsSection.tsx
@@ -0,0 +1,13 @@
+import Section from "../Section"
+
+const SkillsSection = () => {
+    return (
+        <Section>
+            <div className="bg-black text-white">
+                skills
+            </div>
+        </Section>
+    )
+}
+
+export default SkillsSection
