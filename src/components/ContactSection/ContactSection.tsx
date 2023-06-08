a25d669b8cbb235adc8c3b97a56ef8203af18905
TheDeafOne <woodburnkb20@gcc.edu>
2023-06-07 22:21:48 -0500

    base contact section

---
+++ src/components/ContactSection/ContactSection.tsx
@@ -0,0 +1,13 @@
+import Section from "../Section"
+
+const ContactSection = () => {
+    return (
+        <Section>
+            <div className="bg-black text-white">
+                about
+            </div>
+        </Section>
+    )
+}
+
+export default ContactSection
