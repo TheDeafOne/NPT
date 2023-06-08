af8d93b64f842d16bdfeda3a9563ca3f6c780ae7
TheDeafOne <woodburnkb20@gcc.edu>
2023-06-07 22:22:29 -0500

    added all sections to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,23 @@
+import AboutSection from "@/components/AboutSection/AboutSection";
+import ContactSection from "@/components/ContactSection/ContactSection";
+import ExperienceSection from "@/components/ExperienceSection/ExperienceSection";
+import Footer from "@/components/Footer/Footer";
 import Navbar from "@/components/Navbar/Navbar";
+import ProjectsSection from "@/components/ProjectsSection/ProjectsSection";
+import SkillsSection from "@/components/SkillsSection/SkillsSection";
 import WelcomeSection from "@/components/WelcomeSection/WelcomeSection";
 
 export default function Home() {
   return (
-    <div className="bg-black">
+    <div className="bg-white">
       <Navbar />
       <WelcomeSection />
+      <AboutSection />
+      <SkillsSection />
+      <ProjectsSection />
+      <ExperienceSection />
+      <ContactSection />
+      <Footer />
     </div>
   )
 }
