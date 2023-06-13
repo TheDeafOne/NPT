729fa46fda938c5b5e204e89b5cb36ebae01a152
TheDeafOne <woodburnkb20@gcc.edu>
2023-06-12 22:20:41 -0500

    navbar styling and logo addition

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,24 +1,27 @@
 'use client';
+import Image from "next/image";
 import Link from "next/link";
-
+import Logo from '../../resources/icon-light.svg';
 interface INavLink {
     title: string,
     id: string
 }
 
+const handleScroll = (e: any, id: string) => {
+    e.preventDefault();
+    const element = document.getElementById(id);
+    const dims = element!.getBoundingClientRect();
+    const navHeight = document.getElementById("navbar")!.offsetHeight
+    window.scrollTo({
+        top: dims.top - navHeight + window.scrollY,
+        behavior: "smooth"
+    });
+};
+
 const NavLink = ({ title, id }: INavLink) => {
-    const handleScroll = (e: any) => {
-        e.preventDefault();
-        const element = document.getElementById(id);
-        const dims = element!.getBoundingClientRect();
-        const navHeight = document.getElementById("navbar")!.offsetHeight
-        window.scrollTo({
-            top: dims.top - navHeight + window.scrollY, 
-            behavior: "smooth"
-        });
-    };
+
     return (
-        <Link href={`#${id}`} onClick={handleScroll}>
+        <Link id={`${id}-nav`} href={`#${id}`} onClick={(e) => handleScroll(e, id)} className="hover:text-accent text-text text-lg">
             {title}
         </Link>
     )
@@ -33,11 +36,16 @@ const Navbar = () => {
         { title: "Contact Me", id: "contact-section" }
     ]
     return (
-        <nav id="navbar" className="bg-white dark:bg-gray-900 sticky w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600 flex flex-row justify-between">
-            <div>
-                logo
+        <nav id="navbar" className="bg-background sticky w-full z-20 top-0 left-0 flex flex-row justify-between shadow-md">
+            <div className="px-2">
+                <Image
+                    src={Logo}
+                    className="w-[60px] hover:cursor-pointer"
+                    onClick={(e) => handleScroll(e, "welcome-section")}
+                    alt=""
+                />
             </div>
-            <div className="flex flex-row gap-1 px-2">
+            <div className="flex flex-row gap-5 px-10 justify-center items-center">
                 {navItems.map((navItem, i) => {
                     return <NavLink title={navItem.title} id={navItem.id} key={i.toString()} />
                 })}
