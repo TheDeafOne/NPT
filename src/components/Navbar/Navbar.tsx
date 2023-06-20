5a019e937d22f235ab921363dc3ce4460c9ae054
TheDeafOne <woodburnkb20@gcc.edu>
2023-06-19 22:20:25 -0500

    refactor navbar to move NavLink sub-components

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,47 +2,34 @@
 import Image from "next/image";
 import Link from "next/link";
 import Logo from '../../resources/icon-light.svg';
-import { useRef } from "react";
+
+const handleScroll = (e: any, id: string) => {
+    e.preventDefault();
+    const element = document.getElementById(id);
+    const dims = element!.getBoundingClientRect();
+    const navHeight = document.getElementById("navbar")!.offsetHeight
+    window.scrollTo({
+        top: dims.top - navHeight + window.scrollY,
+        behavior: "smooth"
+    });
+};
 
 
 const Navbar = () => {
-    const navRef = useRef<null | HTMLElement>(null);
-    const  welcomeRef = useRef<null | HTMLElement>(null);
-    const  aboutRef = useRef<null | HTMLElement>(null);
-    const  skillsRef = useRef<null | HTMLElement>(null);
-    const  projectsRef = useRef<null | HTMLElement>(null);
-    const  experienceRef = useRef<null | HTMLElement>(null);
-    const  contactRef = useRef<null | HTMLElement>(null);
-
     const navItems = [
-        { title: "Welcome", id: "welcome-section", ref: welcomeRef },
-        { title: "About", id: "about-section", ref: aboutRef },
-        { title: "Skills", id: "skills-section", ref: skillsRef },
-        { title: "Projects", id: "projects-section", ref: projectsRef },
-        { title: "Experience", id: "experience-section", ref: experienceRef },
-        { title: "Contact Me", id: "contact-section", ref: contactRef }
+        { title: "About", id: "about-section" },
+        { title: "Skills", id: "skills-section" },
+        { title: "Projects", id: "projects-section" },
+        { title: "Experience", id: "experience-section" },
+        { title: "Contact Me", id: "contact-section" }
     ]
-
-        
-    const handleScroll = (e: any, ref: any) => {
-        e.preventDefault();
-        const dims = ref.current.getBoundingClientRect();
-        console.log(dims);
-        const navHeight = document.getElementById("navbar")!.offsetHeight
-        window.scrollTo({
-            top: dims.top - navHeight + window.scrollY,
-            behavior: "smooth"
-        });
-    };
-
-
     return (
-        <nav id="navbar" ref={navRef} className="bg-background sticky w-full z-20 top-0 left-0 flex flex-row justify-between shadow-md">
+        <nav id="navbar" className="bg-background sticky w-full z-20 top-0 left-0 flex flex-row justify-between shadow-md">
             <div className="px-2">
                 <Image
                     src={Logo}
                     className="w-[60px] hover:cursor-pointer"
-                    onClick={(e) => handleScroll(e, welcomeRef)}
+                    onClick={(e) => handleScroll(e, "welcome-section")}
                     alt=""
                     priority
                 />
@@ -51,12 +38,12 @@ const Navbar = () => {
                 {navItems.map((navItem, i) => {
                     return (
                         <Link 
+                            title={navItem.title}
+                            key={i} 
                             id={`${navItem.id}-nav`} 
-                            key={i}
                             href={`#${navItem.id}`} 
-                            onClick={(e) => handleScroll(e, navItem.ref)} 
-                            className="hover:text-accent text-text text-lg"
-                            ref={navItem.ref as any}
+                            onClick={(e) => handleScroll(e, navItem.id)} 
+                            className="hover:text-accent text-text text-lg active:text-accent"
                         >
                             {navItem.title}
                         </Link>
