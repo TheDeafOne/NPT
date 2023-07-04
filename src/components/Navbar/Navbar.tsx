ee614d87af042c7d09cea34b98bc20a6411b3880
TheDeafOne <woodburnkb20@gcc.edu>
2023-07-03 19:07:37 -0500

    navbar font changes

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,8 +1,8 @@
 'use client';
 import Image from "next/image";
+import { useEffect, useState } from "react";
 import { Link as ScrollLink, animateScroll } from 'react-scroll';
 import Logo from '../../resources/icon-light.svg';
-import { useEffect, useState } from "react";
 
 const Navbar = () => {
     const [navHeight, setNavHeight] = useState(0)
@@ -11,13 +11,13 @@ const Navbar = () => {
         { title: "Skills", id: "skills-section" },
         { title: "Projects", id: "projects-section" },
         { title: "Experience", id: "experience-section" },
-        { title: "Contact Me", id: "contact-section" }
+        { title: "Kontact Me", id: "contact-section" }
     ]
 
     useEffect(() => {
         setNavHeight(document.getElementById("navbar")!.offsetHeight);
     }, [])
-    
+
     return (
         <nav id="navbar" className="bg-background sticky w-full z-20 top-0 left-0 flex flex-row justify-between shadow-md">
             <div className="px-2">
@@ -37,11 +37,11 @@ const Navbar = () => {
                             key={i}
                             id={`${navItem.id}-nav`}
                             href={`#${navItem.id}`}
-                            className="hover:text-accent text-text text-lg active:text-accent"
+                            className="hover:text-accent text-text font-semibold text-xl font-sans active:text-accent "
                             to={navItem.id}
-                            spy={true} 
-                            smooth={true} 
-                            duration={500} 
+                            spy={true}
+                            smooth={true}
+                            duration={500}
                             offset={-navHeight}
                             activeClass="text-accent"
                         >
