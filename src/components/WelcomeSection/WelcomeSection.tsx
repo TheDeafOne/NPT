7f691533c83c047c35aef8b1ff640ebadbcdc50a
TheDeafOne <woodburnkb20@gcc.edu>
2023-07-04 23:34:18 -0500

    generic name

--- src/components/WelcomeSection/WelcomeSection.tsx
+++ src/components/WelcomeSection/WelcomeSection.tsx
@@ -8,12 +8,16 @@ const WelcomeSection = () => {
         <Section id="welcome-section" className="pt-6">
             <div className="h-screen flex items-center flex-col">
                 <div className="my-[10%]">
-                    <div className="mx-[-2px] my-4">
+                    <div>
                         <span className="text-text text-5xl font-semibold">
                             Hey, I'm
                         </span>
                     </div>
-                    
+                    <div>
+                        <span className="text-text text-8xl font-bold">
+                            Keegan
+                        </span>
+                    </div>
                 </div>
             </div>
         </Section>
