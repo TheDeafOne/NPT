59edbe4968a16749bb69ab95768563e717cd733d
TheDeafOne <woodburnkb20@gcc.edu>
2023-07-23 12:42:16 -0500

    different gradient values

--- src/components/WelcomeSection/WelcomeSection.tsx
+++ src/components/WelcomeSection/WelcomeSection.tsx
@@ -21,8 +21,8 @@ const WelcomeSection = () => {
     return (
         <Section id="welcome-section">
             <GridBackground />
-            <div id="background-mask" className="absolute h-screen w-screen bg-gradient-to-b from-transparent to-background to-95%" />
-            <div className="h-screen flex items-center justify-center flex-col bg-200 animate-gradient-movement bg-gradient-to-r from-background via-primary to-background" ref={sectionRef}>
+            <div id="background-mask" className="absolute h-screen w-screen bg-gradient-to-b from-background from-2% via-transparent to-background to-98%" />
+            <div className="h-screen flex items-center justify-center flex-col bg-200 animate-gradient-movement bg-gradient-to-r from-[#233329] via-[#46683A] to-[#233329]" ref={sectionRef}>
                 <div className='font-lato text-text'>
                     <div className=' relative -top[10px]'>
                         <span className='text-7xl font-bold'>
