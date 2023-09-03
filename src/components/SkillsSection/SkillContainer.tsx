25b17857fdda6ac7cb36afc4ae59a958f784dd53
TheDeafOne <woodburnkb20@gcc.edu>
2023-09-03 13:59:31 -0400

    skills section container

---
+++ src/components/SkillsSection/SkillContainer.tsx
@@ -0,0 +1,10 @@
+import { useEffect } from "react";
+const SkillContainer = ({title}: any) => {
+    return (
+        <div className="bg-black rounded-full h-20 w-20 flex justify-center items-center">
+            {title}
+        </div>
+    )
+}
+
+export default SkillContainer;
--- src/components/SkillsSection/SkillsSection.tsx
+++ src/components/SkillsSection/SkillsSection.tsx
@@ -3,11 +3,8 @@ import Section from "../Section"
 const SkillsSection = () => {
     return (
         <Section id="skills-section">
-            <div className="bg-black text-white h-screen">
-                skills
-                <div className="bg-white text-black">
-                    hey
-                </div>
+            <div className=" text-white h-screen justify-center items-center">
+                test
             </div>
         </Section>
     )
