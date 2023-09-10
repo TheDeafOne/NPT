42ceb1711eae24fd909f992ce818483c22830eee
TheDeafOne <woodburnkb20@gcc.edu>
2023-09-09 21:21:53 -0400

    working images, needs scaled

--- src/components/SkillsSection/SkillContainer.tsx
+++ src/components/SkillsSection/SkillContainer.tsx
@@ -1,8 +1,15 @@
-import { useEffect } from "react";
-const SkillContainer = ({title}: any) => {
+const SkillContainer = ({ title, className, items }: any) => {
+
     return (
-        <div className="bg-black rounded-full h-20 w-20 flex justify-center items-center">
-            {title}
+        <div>
+            <div className={`w-[80px] h-[80px] bg-black rounded-full h-20 w-20 flex justify-center items-center cursor-pointer transform hover:scale-105 ease-in-out duration-100 ${className}`}>
+                {title}
+            </div>
+            {items?.map((logo: any) => {
+                return (
+                    <img src={logo.src} />
+                )
+            })}
         </div>
     )
 }
--- src/components/SkillsSection/SkillsSection.tsx
+++ src/components/SkillsSection/SkillsSection.tsx
@@ -1,10 +1,25 @@
-import Section from "../Section"
+import Section from "../Section";
+import SkillContainer from "./SkillContainer";
+import PythonLogo from '../../resources/logos/languages/python-logo.png';
+import CLogo from '../../resources/logos/languages/C-logo.png';
+import CsharpLogo from '../../resources/logos/languages/Csharp-logo.png';
+import CssLogo from '../../resources/logos/languages/css-logo.png';
+import HTMLLogo from '../../resources/logos/languages/html-logo.png';
+import JavascriptLogo from '../../resources/logos/languages/javascript-logo.png';
+import MATLABLogo from '../../resources/logos/languages/matlab-logo.png';
+import RLogo from '../../resources/logos/languages/R-logo.png';
+import SqlLogo from '../../resources/logos/languages/sql-logo.png';
+import TypescriptLogo from '../../resources/logos/languages/typescript-logo.png';
 
 const SkillsSection = () => {
+    const buttonSize = 50;
+    const languageItems = [PythonLogo, CLogo, CsharpLogo, CssLogo, HTMLLogo, JavascriptLogo, MATLABLogo, RLogo, SqlLogo, TypescriptLogo]
     return (
         <Section id="skills-section">
-            <div className=" text-white h-screen justify-center items-center">
-                test
+            <div className="text-text h-screen flex justify-center items-center">
+                <SkillContainer items={languageItems} title="other" />
+                <SkillContainer title="other" />
+                <SkillContainer title="other" />
             </div>
         </Section>
     )
