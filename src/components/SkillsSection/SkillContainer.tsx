eafe5979c29a9956bf070916166157e3006577c1
TheDeafOne <woodburnkb20@gcc.edu>
2023-09-09 23:21:40 -0400

    language scaling and framing

--- src/components/SkillsSection/SkillContainer.tsx
+++ src/components/SkillsSection/SkillContainer.tsx
@@ -1,13 +1,13 @@
 const SkillContainer = ({ title, className, items }: any) => {
 
     return (
-        <div>
+        <div className="flex justify-center items-center">
             <div className={`w-[80px] h-[80px] bg-black rounded-full h-20 w-20 flex justify-center items-center cursor-pointer transform hover:scale-105 ease-in-out duration-100 ${className}`}>
                 {title}
             </div>
             {items?.map((logo: any) => {
                 return (
-                    <img src={logo.src} />
+                    <img className="h-[50px] absolute" src={logo.src} />
                 )
             })}
         </div>
--- src/components/SkillsSection/SkillsSection.tsx
+++ src/components/SkillsSection/SkillsSection.tsx
@@ -1,5 +1,7 @@
 import Section from "../Section";
 import SkillContainer from "./SkillContainer";
+
+// language logos
 import PythonLogo from '../../resources/logos/languages/python-logo.png';
 import CLogo from '../../resources/logos/languages/C-logo.png';
 import CsharpLogo from '../../resources/logos/languages/Csharp-logo.png';
@@ -11,12 +13,14 @@ import RLogo from '../../resources/logos/languages/R-logo.png';
 import SqlLogo from '../../resources/logos/languages/sql-logo.png';
 import TypescriptLogo from '../../resources/logos/languages/typescript-logo.png';
 
+
+
 const SkillsSection = () => {
     const buttonSize = 50;
     const languageItems = [PythonLogo, CLogo, CsharpLogo, CssLogo, HTMLLogo, JavascriptLogo, MATLABLogo, RLogo, SqlLogo, TypescriptLogo]
     return (
         <Section id="skills-section">
-            <div className="text-text h-screen flex justify-center items-center">
+            <div className="text-text h-screen flex flex-row justify-center items-center">
                 <SkillContainer items={languageItems} title="other" />
                 <SkillContainer title="other" />
                 <SkillContainer title="other" />
