74ea6fdc0a9eb905bf8ca1979f6427285a7bbec1
TheDeafOne <woodburnkb20@gcc.edu>
2023-09-12 11:31:12 -0400

    ongoing skills adjustments

--- src/components/SkillsSection/SkillContainer.tsx
+++ src/components/SkillsSection/SkillContainer.tsx
@@ -1,13 +1,55 @@
-const SkillContainer = ({ title, className, items }: any) => {
+import anime from "animejs"
+import { useEffect } from "react";
+
+const SkillContainer = ({ title, className, id, items }: any) => {
+    useEffect(() => {
+    }, [])
+
+    const showIcons = (e: any) => {
+        // console.log(e);
+        const itemsBar = Number(items.length/2);
+        let itemsPointer = 0;
+        let yPos = -60;
+        let xPos = -20;
+        anime({
+            targets:  `#${id}-logo`,
+            // opacity: [0,1],
+            // borderRadius: ()=> {
+            //   return anime.random(25,50);
+            // },
+            translateY: () => {
+                if (itemsPointer % itemsBar == 0) {
+                    yPos += 20;
+                    xPos = -20;
+                }
+                itemsPointer += 1;
+                return yPos + 'vh';
+            },
+            translateX: ()=> {
+                xPos += 10;
+                return xPos + 'vw';
+            },
+            // scale: ()=> {
+            //   return anime.random(0.8,1.2);
+            // },
+            duration: ()=> {
+              return anime.random(1000,1500);
+            },
+            delay: anime.stagger(50),
+            loop: false,
+            direction: "alternate",
+            easing: "easeOutExpo"
+        });
+    }
 
     return (
-        <div className="flex justify-center items-center">
-            <div className={`w-[80px] h-[80px] bg-black rounded-full h-20 w-20 flex justify-center items-center cursor-pointer transform hover:scale-105 ease-in-out duration-100 ${className}`}>
+        <div className="flex justify-center items-center" onClick={(e) => showIcons(e)}>
+            <div className={`w-[80px] h-[80px] bg-black rounded-full h-20 w-20 z-10 flex text-center justify-center items-center cursor-pointer transform hover:scale-105 ease-in-out duration-100 ${className}`}>
                 {title}
             </div>
-            {items?.map((logo: any) => {
+            {items?.map((logo: any, i: any) => {
                 return (
-                    <img className="h-[50px] absolute" src={logo.src} />
+                    <img key={i} id={`${id}-logo`} className="h-[50px] absolute" src={logo.src} />
                 )
             })}
         </div>
--- src/components/SkillsSection/SkillsSection.tsx
+++ src/components/SkillsSection/SkillsSection.tsx
@@ -8,22 +8,35 @@ import CsharpLogo from '../../resources/logos/languages/Csharp-logo.png';
 import CssLogo from '../../resources/logos/languages/css-logo.png';
 import HTMLLogo from '../../resources/logos/languages/html-logo.png';
 import JavascriptLogo from '../../resources/logos/languages/javascript-logo.png';
+import JavaLogo from '../../resources/logos/languages/java-logo.png';
 import MATLABLogo from '../../resources/logos/languages/matlab-logo.png';
 import RLogo from '../../resources/logos/languages/R-logo.png';
 import SqlLogo from '../../resources/logos/languages/sql-logo.png';
 import TypescriptLogo from '../../resources/logos/languages/typescript-logo.png';
 
+// framework logos
+import AspNetLogo from '../../resources/logos/frameworks/aspnet-logo.png';
+import FlaskLogo from '../../resources/logos/frameworks/flask-logo.png';
+import JavaFXLogo from '../../resources/logos/frameworks/javafx-logo.png';
+import JUnitLogo from '../../resources/logos/frameworks/junit-logo.png';
+import MochaLogo from '../../resources/logos/frameworks/mocha-logo.png';
+import NodeJSLogo from '../../resources/logos/frameworks/nodejs-logo.png';
+import NumpyLogo from '../../resources/logos/frameworks/numpy-logo.png';
+import PandasLogo from '../../resources/logos/frameworks/pandas-logo.png';
+import PuppeteerLogo from '../../resources/logos/frameworks/puppeteer-logo.png';
+import ReactLogo from '../../resources/logos/frameworks/react-logo.png';
+import SpringbootLogo from '../../resources/logos/frameworks/springboot-logo.png';
 
 
 const SkillsSection = () => {
-    const buttonSize = 50;
-    const languageItems = [PythonLogo, CLogo, CsharpLogo, CssLogo, HTMLLogo, JavascriptLogo, MATLABLogo, RLogo, SqlLogo, TypescriptLogo]
+    const languageItems = [PythonLogo, JavaLogo, CLogo, CsharpLogo, RLogo, CssLogo, HTMLLogo, JavascriptLogo, TypescriptLogo, SqlLogo]
+    const frameworkItems = [ReactLogo, SpringbootLogo, AspNetLogo, FlaskLogo, NodeJSLogo, JavaFXLogo, JUnitLogo, MochaLogo, PuppeteerLogo, NumpyLogo, PandasLogo]
     return (
         <Section id="skills-section">
-            <div className="text-text h-screen flex flex-row justify-center items-center">
-                <SkillContainer items={languageItems} title="other" />
-                <SkillContainer title="other" />
-                <SkillContainer title="other" />
+            <div className="text-text h-screen flex flex-row space-x-10 justify-center items-center">
+                <SkillContainer items={languageItems} id="languages" title="Languages" />
+                <SkillContainer items={frameworkItems} id="landf" title="Libraries & Frameworks" />
+                <SkillContainer id="tandp" title="Tools & Platforms" />
             </div>
         </Section>
     )
